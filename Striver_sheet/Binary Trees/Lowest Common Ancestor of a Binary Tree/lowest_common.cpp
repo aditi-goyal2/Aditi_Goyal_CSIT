0e2613f66acaa9b167c29c0e8b4bdb9471494447
aditi-goyal2 <aditi.1923csi1024@kiet.edu>
2022-07-27 03:58:53 +0530

    week 3

--- Striver_sheet/Binary Trees/Lowest Common Ancestor of a Binary Tree/lowest_common.cpp
+++ Striver_sheet/Binary Trees/Lowest Common Ancestor of a Binary Tree/lowest_common.cpp
@@ -1,3 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
@@ -16,3 +29,8 @@ public:
             return right;
     }
 };
+int main()
+{
+
+    return 0;
+}
